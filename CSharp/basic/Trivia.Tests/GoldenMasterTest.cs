643a230a918ab2ed6772fcf4dedae3bf9ce0c54c
Jeremy LAIR <Jeremy.LAIR@vinci-energies.net>
2020-11-11 12:05:05 +0100

    add golden master test 20 times

---
+++ CSharp/basic/Trivia.Tests/GoldenMasterTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Trivia.Tests
+{
+	[TestClass]
+	public class GoldenMasterTest
+	{
+		[TestMethod]
+		public void GoldenMaster()
+		{
+			const string outputPath = "Output.txt";
+
+			if (File.Exists(outputPath))
+				File.Delete(outputPath);
+
+			using (var fileStream = new FileStream(outputPath, FileMode.CreateNew, FileAccess.Write))
+			{
+				using (var streamWriter = new StreamWriter(fileStream))
+				{
+					TextWriter oldOut = Console.Out;
+					Console.SetOut(streamWriter);
+
+					for (int i = 0; i < 20; i++)
+					{
+						Console.WriteLine();
+						GameRunner.Main(null);
+					}
+
+					Console.SetOut(oldOut);
+				}
+			}
+
+			Assert.AreEqual(File.ReadAllText(outputPath), File.ReadAllText("Expected.txt"));
+		}
+	}
+}
--- CSharp/basic/Trivia/GameRunner.cs
+++ CSharp/basic/Trivia/GameRunner.cs
@@ -2,42 +2,43 @@
 
 namespace Trivia
 {
-    public class GameRunner
-    {
+	public class GameRunner
+	{
+		private static readonly Random Rand = new Random(new Guid("1BEFC143-CBA2-4F3D-9219-F2220F792D28").GetHashCode());
 
-        private static bool notAWinner;
+		private static bool notAWinner;
 
-        public static void Main(String[] args)
-        {
-            Game aGame = new Game();
+		public static void Main(String[] args)
+		{
+			Game aGame = new Game();
 
-            aGame.Add("Chet");
-            aGame.Add("Pat");
-            aGame.Add("Sue");
+			aGame.Add("Chet");
+			aGame.Add("Pat");
+			aGame.Add("Sue");
 
-            Random rand = new Random();
+			Random rand = Rand;
 
-            do
-            {
+			do
+			{
 
-                aGame.Roll(rand.Next(5) + 1);
+				aGame.Roll(rand.Next(5) + 1);
 
-                if (rand.Next(9) == 7)
-                {
-                    notAWinner = aGame.WrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.WasCorrectlyAnswered();
-                }
-
-
-
-            } while (notAWinner);
-
-        }
+				if (rand.Next(9) == 7)
+				{
+					notAWinner = aGame.WrongAnswer();
+				}
+				else
+				{
+					notAWinner = aGame.WasCorrectlyAnswered();
+				}
 
 
-    }
+
+			} while (notAWinner);
+
+		}
+
+
+	}
 
 }
